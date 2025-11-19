5e67cb9beb99b53587f0c407ab35a1049de67db9
lada <ladaapp@protonmail.com>
2025-11-19 13:14:21 +0000

    packaging: Create windows packaging script to automate most release steps

--- packaging/windows/README.md
+++ packaging/windows/README.md
@@ -1,91 +1,38 @@
-## System preparation
-Prerequisite is an environment set up as described in [Windows build instructions](../../docs/windows_install.md).
-
-## Package a new version
-
-Summary: Setup new temporary venv, checkout lada and install it.
-
-TODO: Like we do for Docker and Flatpak packages we should pin the dependencies
+## Build exe
 
 ```powershell
-$project = "C:\project"
-cd $project
-
-git clone https://codeberg.org/ladaapp/lada.git -b v0.8.2 release_lada
-cd release_lada
-
-py -m venv .venv
-.\.venv\Scripts\Activate.ps1
-
-$env:Path = $project + "\build\gtk\x64\release\bin;" + $project + "\gettext\bin;" + $env:Path
-$env:LIB = $project + "\build\gtk\x64\release\lib;" + $env:LIB
-$env:INCLUDE = $project + "\build\gtk\x64\release\include;" + $project + "\build\gtk\x64\release\include\cairo;" + $project + "\build\gtk\x64\release\include\glib-2.0;" + $project + "\build\gtk\x64\release\include\gobject-introspection-1.0;" + $project + "\build\gtk\x64\release\lib\glib-2.0\include;" + $env:INCLUDE
-
-pip install torch==2.8.0 torchvision==0.23.0 --index-url https://download.pytorch.org/whl/cu128
-pip install av==15.1.0
-pip install '.[basicvsrpp]'
-
-pip install --force-reinstall (Resolve-Path ($project + "\build\gtk\x64\release\python\pygobject*.whl"))
-pip install --force-reinstall (Resolve-Path ($project + "\build\gtk\x64\release\python\pycairo*.whl"))
-
-pip install patch
-python -m patch -p5 -d .venv/lib/site-packages patches/increase_mms_time_limit.patch
-python -m patch -p5 -d .venv/lib/site-packages patches/remove_ultralytics_telemetry.patch
-python -m patch -p5 -d .venv/lib/site-packages patches/fix_loading_mmengine_weights_on_torch26_and_higher.diff
-pip uninstall patch
-
-Invoke-WebRequest 'https://huggingface.co/ladaapp/lada/resolve/main/lada_mosaic_detection_model_v3.1_accurate.pt?download=true' -OutFile ".\model_weights\lada_mosaic_detection_model_v3.1_accurate.pt"
-Invoke-WebRequest 'https://huggingface.co/ladaapp/lada/resolve/main/lada_mosaic_detection_model_v3.1_fast.pt?download=true' -OutFile ".\model_weights\lada_mosaic_detection_model_v3.1_fast.pt"
-Invoke-WebRequest 'https://huggingface.co/ladaapp/lada/resolve/main/lada_mosaic_detection_model_v2.pt?download=true' -OutFile ".\model_weights\lada_mosaic_detection_model_v2.pt"
-Invoke-WebRequest 'https://huggingface.co/ladaapp/lada/resolve/main/lada_mosaic_restoration_model_generic_v1.2.pth?download=true' -OutFile ".\model_weights\lada_mosaic_restoration_model_generic_v1.2.pth"
-Invoke-WebRequest 'https://drive.usercontent.google.com/download?id=1ulct4RhRxQp1v5xwEmUH7xz7AK42Oqlw&export=download&confirm=t' -OutFile ".\model_weights\3rd_party\clean_youknow_video.pth"
-
-powershell .\translations/compile_po.ps1 --release
+powershell -ExecutionPolicy Bypass ./packaging/windows/package_executable.ps1
 ```
 
-Just do a quick test `lada`, drop in a video and see if it loads. If all looks good lets continue and create a package:
+Script options:
+* `--skip-winget`: Skip installing/upgrading system dependencies via winget
+* `--skip-gvsbuild`: Skip installing/upgrading system dependencies via gvsbuild
+* `--clean-gvsbuild`: Does a clean build of gvsbuild
+* `--cli-only`: Skips gvsbuild and builds only `lada-cli.exe`
 
-```powershell
-pip install pyinstaller 
-```
+> [!TIP]
+> If you updated `gvsbuild`, `uv` or `python` do a clean build (`--clean-gvsbuild`)
 
-On my Windows build machine I got a crash of pyinstaller caused by polars dependency pulled in by ultralytics.
-Problem seems to be that it expects AVX512 capable CPU which this machine doesn't offer. Fortunately there is an alternative package:
+> [!TIP]
+> If you get a build error about *Clock skew* check you Date & Time settings. If it doesn't work rewrite timestamps:
+> ```Powershell
+> $now = (Get-Date)
+> Get-ChildItem -Path ./build_gtk_release/build -Recurse | ForEach-Object { $_.LastWriteTime = $now }
+> ```
 
-```powershell
-pip uninstall polars
-pip install polars-lts-cpu
-```
-Now we can build the package using pyinstaller:
+> [!TIP]
+> After doing major packaging changes that involve system dependencies test the exe on another pristine Windows VM.
+> 
+> This makes sure that PyInstaller picked up all required dependencies which are available on the build machine but maybe aren't on the users Windows installation.
 
-```powershell
-pyinstaller ./packaging/windows/lada.spec
-```
+## Publish exe
 
-This will create a `dist` directory in the project root.
+* Attach both `lada-<version>.7z.001` and `lada-<version>.7z.002` to GitHub Release (rag-and-drop to the Draft Release) 
+* Upload `lada-<version>.7z` to https://pixeldrain.com
+* Add Pixeldrain download link and link to GitHub Release to both Draft Releases on GitHub and Codeberg
 
-Copy this over to another pristine Windows VM for testing if any of the dependencies changed.
-This machine would not have any of the environment changes, libraries and binaries of the build machine so it can be used to test if all necessary dependencies are bundled correctly.
+## Update python dependencies
 
-If the dependencies did not change (changes just within lada package) then testing on the same build machine should be fine.
+The `requirements.txt` file is generated by `uv export` based on `uv.lock` located in the root of the project.
 
-If all looks good we can zip it up and upload it to GitHub:
-
-* Open 7-Zip
-* Select the `lada` directory within `dist` directory created by pyinstaller.
-* Add
-* Set `Archive format` to 7z
-* Set `Archive filename` to lada-<version>.7z
-* Set `Split to volume, bytes` to 1999M # TODO: maybe not necessary now that we're not bound by the 2GB limit of GitHub
-* OK
-
-Then calculate the files' checksum
-
-```powershell
-Get-FileHash -Algorithm SHA256 .\path\to\lada-<version>.7z
-```
-
-Copy the hash into a file named `lada-<version>.sha256.txt`
-It should contain a single line with checksum followed by a space and then the .7z file name
-
-Attach these files then to the Release on GitHub (drag-and-drop to the Draft Release)
+See packaging [README.md](../README.md) for context.
---
+++ packaging/windows/package_executable.ps1
@@ -0,0 +1,210 @@
+# SPDX-FileCopyrightText: Lada Authors
+# SPDX-License-Identifier: AGPL-3.0
+
+$global:PYINSTALLER_VERSION = "6.16.0"
+$global:GVSBUILD_VERSION = "2025.10"
+$global:PYTHON_VERSION = "3.13"
+$global:UV_VERSION = "0.9.10"
+
+function Ask-YesNo {
+    param([Parameter(Mandatory)] [string]$Question)
+
+    while ($true) {
+        $response = Read-Host "$Question (Y/N)"
+
+        switch ($response.ToUpper()) {
+            'Y' { return $true }
+            'N' { return $false }
+            default { Write-Host "Please enter Y or N." -ForegroundColor Yellow }
+        }
+    }
+}
+
+function Install-SystemDependencies {
+    Write-Host "Installing system dependencies..."
+
+    winget install --id Gyan.FFmpeg -e --source winget
+    winget install --id Git.Git -e --source winget
+    winget install --id=astral-sh.uv -e --source winget --version $global:UV_VERSION --force
+    winget install --id MSYS2.MSYS2 -e --source winget
+    winget install --id Microsoft.VisualStudio.2022.BuildTools -e --source winget --silent --override "--wait --quiet --add ProductLang En-us --add Microsoft.VisualStudio.Workload.VCTools --includeRecommended"
+    winget install --id Rustlang.Rustup -e --source winget
+    winget install --id Microsoft.VCRedist.2013.x64  -e --source winget
+    winget install --id Microsoft.VCRedist.2013.x86  -e --source winget
+    winget install --id=7zip.7zip -e --source winget
+}
+
+function Build-SystemDependencies {
+    param([Parameter(Mandatory)] [boolean]$clean)
+
+    Write-Host "Building system dependencies..."
+
+    uv venv --clear --python $global:PYTHON_VERSION venv_gtk_release
+    .\venv_gtk_release\Scripts\Activate.ps1
+
+    uv pip install gvsbuild==$global:GVSBUILD_VERSION
+    uv pip install patch
+    uv run --no-project python -m patch -p1 -d venv_gtk_release/lib/site-packages patches/gvsbuild_gstreamer_gtk4_plugin.patch
+    uv run --no-project python -m patch -p1 -d venv_gtk_release/lib/site-packages patches/gvsbuild_ffmpeg.patch
+    uv pip uninstall patch
+
+    $cleanArgument = if ($clean) { '--clean' } else { '' }
+
+    gvsbuild build `
+        --configuration=release `
+        $cleanArgument `
+        --build-dir='./build_gtk_release' `
+        --enable-gi `
+        --py-wheel `
+        gtk4 adwaita-icon-theme pygobject libadwaita gstreamer gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-plugin-gtk4 gst-libav gst-python gettext
+
+    deactivate
+}
+
+function Compile-Translations {
+    Write-Host "Compiling translations..."
+    # Adjust PATH to include gettext tools used in translation compile script
+    $path_backup = $env:Path
+    $gtk_bin_dir = (Resolve-Path ".\build_gtk_release\gtk\x64\release\bin").Path
+    $env:Path = $gtk_bin_dir + ";" + $env:Path
+
+    & ./translations/compile_po.ps1 --release
+
+    $env:Path = $path_backup
+}
+
+function Download-ModelWeights {
+    Write-Host "Downloading model weights..."
+
+    function Is-Downloaded($file_name, $sha256) {
+        $path = ".\model_weights\" + $file_name
+        return (Test-Path $path) -And ((Get-FileHash -Algorithm SHA256 $path).Hash -eq $sha256)
+    }
+
+    function Download($url, $file_name, $sha256) {
+        if (Is-Downloaded $file_name $sha256) {
+            return
+        }
+        Invoke-WebRequest $url -OutFile (".\model_weights\" + $file_name)
+        if (!(Is-Downloaded $file_name $sha256)) {
+            Write-Warning "Error downloading " + $url
+            exit 1
+        }
+    }
+
+    Download 'https://huggingface.co/ladaapp/lada/resolve/main/lada_mosaic_detection_model_v3.1_accurate.pt?download=true' "lada_mosaic_detection_model_v3.1_accurate.pt" "2b6e5d6cd5a795a4dcc1205b817a7323a4bd3725cef1a7de3a172cb5689f0368"
+    Download 'https://huggingface.co/ladaapp/lada/resolve/main/lada_mosaic_detection_model_v3.1_fast.pt?download=true' "lada_mosaic_detection_model_v3.1_fast.pt" "25d62894c16bba00468f3bcc160360bb84726b2f92751b5e235578bf2f9b0820"
+    Download 'https://huggingface.co/ladaapp/lada/resolve/main/lada_mosaic_detection_model_v2.pt?download=true' "lada_mosaic_detection_model_v2.pt" "056756fcab250bcdf0833e75aac33e2197b8809b0ab8c16e14722dcec94269b5"
+    Download 'https://huggingface.co/ladaapp/lada/resolve/main/lada_mosaic_restoration_model_generic_v1.2.pth?download=true' "lada_mosaic_restoration_model_generic_v1.2.pth" "d404152576ce64fb5b2f315c03062709dac4f5f8548934866cd01c823c8104ee"
+    Download 'https://drive.usercontent.google.com/download?id=1ulct4RhRxQp1v5xwEmUH7xz7AK42Oqlw&export=download&confirm=t' "3rd_party\clean_youknow_video.pth" "5643ca297c13920b8ffd39a0d85296e494683a69e5e8204d662653d24c582766"
+}
+
+function Install-PythonDependencies {
+    Write-Host "Installing Python dependencies..."
+
+    uv venv --clear --python $global:PYTHON_VERSION venv_release_win
+    .\venv_release_win\Scripts\Activate.ps1
+
+    $release_dir = (Resolve-Path ".\build_gtk_release\gtk\x64\release").Path
+
+    uv pip install --no-deps --requirement packaging/windows/requirements.txt --extra-index-url https://download.pytorch.org/whl/cu128 --index-strategy unsafe-best-match
+    # Fix crash due to polars package requiring AVX512 CPU which isn't available on my build machine (use legacy version)
+    # This dependency is not used by Lada. It gets pulled in by ultralytics which uses it outside of inferencing paths
+    uv pip uninstall polars
+    uv pip install polars-lts-cpu
+
+    uv pip install --force-reinstall (Resolve-Path ".\build_gtk_release\gtk\x64\release\python\pygobject*.whl").Path
+    uv pip install --force-reinstall (Resolve-Path ".\build_gtk_release\gtk\x64\release\python\pycairo*.whl").Path
+
+    uv pip install --no-deps '.[gui]'
+
+    uv pip install pyinstaller==$global:PYINSTALLER_VERSION
+
+    uv pip install patch
+    uv run --no-project python -m patch -p1 -d .venv/lib/site-packages patches/increase_mms_time_limit.patch
+    uv run --no-project python -m patch -p1 -d .venv/lib/site-packages patches/remove_ultralytics_telemetry.patch
+    uv run --no-project python -m patch -p1 -d .venv/lib/site-packages patches/fix_loading_mmengine_weights_on_torch26_and_higher.diff
+    uv pip uninstall patch
+
+    deactivate
+}
+
+function Create-EXE {
+    param([Parameter(Mandatory)] [boolean]$cli_only)
+
+    Write-Host "Creating executable..."
+
+    .\venv_release_win\Scripts\Activate.ps1
+
+    $release_dir = (Resolve-Path ".\build_gtk_release\gtk\x64\release").Path
+    $env:Path = $release_dir + "\bin;" + $env:Path
+    $env:LIB = $release_dir + "\lib;" + $env:LIB
+    $env:INCLUDE = $release_dir + "\include;" + $release_dir + "\include\cairo;" + $release_dir + "\include\glib-2.0;" + $release_dir + "\include\gobject-introspection-1.0;" + $release_dir + "\lib\glib-2.0\include;" + $env:INCLUDE
+
+    $cli_only_arg = if ($cli_only) { '--cli-only' } else { '' }
+
+    uv run --no-project pyinstaller --noconfirm ./packaging/windows/lada.spec -- $cli_only_arg
+
+    deactivate
+}
+
+function Create-7ZArchive {
+    Write-Host "Creating 7z archive..."
+
+    .\venv_release_win\Scripts\Activate.ps1
+    $version = $(uv run --no-project python -c 'import lada; print(lada.VERSION)')
+    deactivate
+
+    $env:Path = ($env:Programfiles + "\7-Zip;") + $env:Path
+
+    $archive_path = "./dist/" + "lada-" + $version + ".7z"
+    $tmp_archive_path = "./dist/" + "lada-" + $version + ".tmp.7z"
+
+    # Delete files from prior runs
+    Get-ChildItem "./dist" -filter "*.7z*" | ForEach-Object {
+        rm $_.FullName
+    }
+
+    # Create single-file .7z archive
+    7z.exe a $tmp_archive_path "./dist/lada/*"
+
+    # Split .7z archive into 2GB chunks so they can be uploaded to GitHub Releases
+    7z.exe a -v1999m $archive_path "./dist/lada/*"
+
+    mv $tmp_archive_path $archive_path
+
+    Get-ChildItem "./dist" -filter "*.7z*" | ForEach-Object {
+        $sha256 = (Get-FileHash -Algorithm SHA256 $_.FullName).Hash.ToLower()
+        echo ($sha256 + " " + $_.Basename + $_.Extension) > ($_.FullName + ".sha256")
+    }
+}
+
+function Check-ProjectRoot {
+    if (!(Test-Path ".\pyproject.toml")) {
+        Write-Warning "This script needs to be run from the project root directory."
+        exit 1
+    }
+}
+
+# ---------------------
+# EXECUTE PACKAGING STEPS
+# ---------------------
+
+$ErrorActionPreference = "Stop"
+
+Check-ProjectRoot
+
+if ($args -notcontains "--skip-winget") {
+    Install-SystemDependencies
+    if (!(Ask-YesNo "Installing/Upgrading winget programs finished. Check the winget install output above. You may want to stop and restart this script in a new shell for certain installs/updates. Do you want to continue?")) {
+        exit 0
+    }
+}
+if (($args -notcontains "--skip-gvsbuild") -Or ($args -notcontains "--cli-only")) {
+    Build-SystemDependencies ($args -contains "--clean-gvsbuild")
+}
+Compile-Translations
+Download-ModelWeights
+Install-PythonDependencies
+Create-EXE ($args -contains "--cli-only")
+Create-7ZArchive
