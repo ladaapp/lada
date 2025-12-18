86c6bb4aff08e9de8c57225c98082af8de0603de
ladaapp <ladaapp@protonmail.com>
2025-12-18 18:20:19 +0000

    CLI+GUI: Tweak documentation of face detection feature to include that it may worsen regular NSFW mosaic detection

--- lada/cli/main.py
+++ lada/cli/main.py
@@ -62,7 +62,7 @@ def setup_argparser() -> argparse.ArgumentParser:
     group_general.add_argument('--help', action='store_true', help=_("Show this help message and exit"))
 
     export = parser.add_argument_group(_('Export'))
-    export.add_argument('--encoding-preset', type=str, default="h264-cpu-hq", help=_('Select encoding preset by name. Ignored if "--encoder" and "--encoder-options" are used. (default: %(default)s)'))
+    export.add_argument('--encoding-preset', type=str, default="h264-cpu-hq", help=_('Select encoding preset by name. Use "--list-encoding-presets" to see what\'s available. Ignored if "--encoder" and "--encoder-options" are used (default: %(default)s)'))
     export.add_argument('--list-encoding-presets', action='store_true', help=_("List available encoding presets and exit"))
     export.add_argument('--encoder', type=str, help=_('Select video encoder by name. Use "--list-encoders" to see what\'s available. (default: %(default)s)'))
     export.add_argument('--list-encoders', action='store_true', help=_("List available encoders and exit"))
@@ -79,7 +79,7 @@ def setup_argparser() -> argparse.ArgumentParser:
     group_detection = parser.add_argument_group(_('Mosaic Detection'))
     group_detection.add_argument('--mosaic-detection-model', type=str, default='v4-fast', help=_('Name of detection model or path to model weights file. Use "--list-mosaic-detection-models" to see what\'s available. (default: %(default)s)'))
     group_detection.add_argument('--list-mosaic-detection-models', action='store_true', help=_("List available detection models found in model weights directory and exit (default location is './model_weights' if not overwritten by environment variable LADA_MODEL_WEIGHTS_DIR)"))
-    group_detection.add_argument('--detect-face-mosaics', action=argparse.BooleanOptionalAction, default=False, help=_("Detect and ignore areas of pixelated faces. Prevents restoration artifacts if the source includes these types of mosaics. Available for models v3 and newer. (default: %(default)s)"))
+    group_detection.add_argument('--detect-face-mosaics', action=argparse.BooleanOptionalAction, default=False, help=_("Detect and ignore areas of pixelated faces. Can prevent restoration artifacts but may worsen detection of NSFW mosaics. Available for models v3 and newer. (default: %(default)s)"))
 
     return parser
 
