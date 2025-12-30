53d94153c7dcc57036dbe8dfb7de2c22c8419d31
ladaapp <ladaapp@protonmail.com>
2025-12-30 14:27:43 +0000

    packaging: Fix Export Preview button not working in Flatpak
    Overwrite TMPDIR env var choosing a path that is both accessible by sandbox and host so Preview button can launch media player on the host using this path

--- lada/__init__.py
+++ lada/__init__.py
@@ -15,6 +15,12 @@ VERSION = '0.9.1-dev'
 
 LOG_LEVEL = os.environ.get("LOG_LEVEL", "WARNING")
 
+is_running_inside_flatpak = "FLATPAK_ID" in os.environ and "XDG_RUNTIME_DIR" in os.environ
+if is_running_inside_flatpak and "TMPDIR" not in os.environ:
+    # The path $XDG_RUNTIME_DIR/app/$FLATPAK_ID should be accessible from inside the sandbox as well as from the host
+    # We need this so that the media player launched on the host is able to access the file if .mp4 Fast start is enabled and the Export Preview button is clicked.
+    os.environ["TMPDIR"] = os.path.join(os.environ["XDG_RUNTIME_DIR"], "app", os.environ["FLATPAK_ID"])
+
 @dataclass(frozen=True)
 class ModelFile:
     name: str
