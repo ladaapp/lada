ladaapp/lada

commit 314fc1e6c5fe1cd26d34128fb138e796acd2d7e9

packaging: Create Intel Arc and Nvidia Flatpak extensions (#303)
First leg of implementing #299 (Windows packaging changes will be done in a follow-up change)

Breaking change for existing Flatpak users as the existing (main) Flatpak would not contain torch installation anymore after this change and users are required to install the compatible extension for their hardware (Intel Arc or Nvidia). For reference this should then looks similar to OBS which also uses Flatpak extensions https://flathub.org/en/apps/com.obsproject.Studio

If the app io.github.ladaapp.lada is run but the user forgot to install the Extension then they will be presented with a view telling them to install one of the extensions in order to use the app. Similarly for CLI, a notice is printed on the console if torch is not found asking the user to install a flatpak extension compatible with their hardware.

Reviewed-on: https://codeberg.org/ladaapp/lada/pulls/303

packaging/windows/lada.spec
@@ -47,7 +47,7 @@ def get_gui_components(project_root_dir: str, common_datas: list, common_binarie
     ] + [
         (ospj(project_root_dir, 'lada/gui/style.css'), '.'),
         (ospj(project_root_dir, 'lada/gui/resources.gresource'), '.'),
-        (ospj(project_root_dir, 'packaging/flatpak/share/io.github.ladaapp.lada.png'), 'share/icons/hicolor/128x128/apps'),
+        (ospj(project_root_dir, 'assets/io.github.ladaapp.lada.png'), 'share/icons/hicolor/128x128/apps'),
     ]
 
     gtk_release_dir = pathlib.Path(project_root_dir) / "build_gtk_release" / "gtk" / "x64" / "release"
@@ -184,7 +184,7 @@ if not args.cli_only:
 common_datas = get_common_datas(project_root)
 common_binaries = get_common_binaries()
 common_runtime_hooks = [ospj(project_root, "packaging/windows/pyinstaller_runtime_hook_lada.py")]
-common_icon = [ospj(project_root, 'packaging/flatpak/share/io.github.ladaapp.lada.png')]
+common_icon = [ospj(project_root, 'assets/io.github.ladaapp.lada.png')]
 
 cli_a, cli_pyz, cli_exe = get_cli_components(project_root, common_datas, common_binaries, common_runtime_hooks, common_icon)
 coll = COLLECT(

lada/__init__.py
@@ -30,8 +30,8 @@ VERSION = _get_version('0.10.2.dev')
 
 LOG_LEVEL = os.environ.get("LOG_LEVEL", "WARNING")
 
-is_running_inside_flatpak = "FLATPAK_ID" in os.environ and "XDG_RUNTIME_DIR" in os.environ
-if is_running_inside_flatpak and "TMPDIR" not in os.environ:
+IS_FLATPAK = "FLATPAK_ID" in os.environ and "XDG_RUNTIME_DIR" in os.environ
+if IS_FLATPAK and "TMPDIR" not in os.environ:
     # The path $XDG_RUNTIME_DIR/app/$FLATPAK_ID should be accessible from inside the sandbox as well as from the host
     # We need this so that the media player launched on the host is able to access the file if .mp4 Fast start is enabled and the Export Preview button is clicked.
     os.environ["TMPDIR"] = os.path.join(os.environ["XDG_RUNTIME_DIR"], "app", os.environ["FLATPAK_ID"])

lada/cli/main.py
@@ -8,7 +8,16 @@ import sys
 import tempfile
 import textwrap
 
-import torch
+try:
+    import torch
+except ModuleNotFoundError:
+    from lada import IS_FLATPAK
+    if IS_FLATPAK:
+        print(_("No GPU Add-On installed"))
+        print(_("In order to use the application you need to install one of Lada's Flatpak Add-Ons that is compatible with your hardware"))
+        sys.exit(1)
+    else:
+        raise
 
 from lada import VERSION, ModelFiles
 from lada.cli import utils

lada/gui/application.py
@@ -1,0 +1,107 @@
+# SPDX-FileCopyrightText: Lada Authors
+# SPDX-License-Identifier: AGPL-3.0
+
+import logging
+import pathlib
+
+import gi
+
+from lada import VERSION, LOG_LEVEL
+
+gi.require_version('Gtk', '4.0')
+gi.require_version('Adw', '1')
+gi.require_version('Gst', '1.0')
+gi.require_version('GstApp', '1.0')
+
+from gi.repository import Gtk, Gio, Adw, Gdk, Gst, GObject
+
+Gst.init(None)
+
+here = pathlib.Path(__file__).parent.resolve()
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=LOG_LEVEL)
+
+from lada.gui.window import MainWindow
+from lada.gui.shortcuts import ShortcutsWindow, ShortcutsManager
+from lada.gui.config.config import Config
+
+class LadaApplication(Adw.Application):
+
+    def __init__(self):
+        super().__init__(application_id='io.github.ladaapp.lada',
+                         flags=Gio.ApplicationFlags.DEFAULT_FLAGS)
+        self.create_action("quit", lambda *_: self.quit(), ("<primary>q",))
+        self.create_action('about', self.on_about_action)
+        self.create_action('shortcuts', self.on_shortcut_action)
+
+        css_provider = Gtk.CssProvider()
+        css_provider.load_from_path(str(here.joinpath('style.css')))
+        Gtk.StyleContext.add_provider_for_display(Gdk.Display.get_default(), css_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+
+        resource = Gio.resource_load(str(here.joinpath('resources.gresource')))
+        Gio.resources_register(resource)
+
+        self._shortcuts_manager: ShortcutsManager = ShortcutsManager()
+        self._config: Config = Config(self.get_style_manager())
+        self._config.load_config()
+        self.window: MainWindow | None = None
+
+        self.connect("shutdown", self.on_shutdown)
+
+    @GObject.Property(type=ShortcutsManager)
+    def shortcuts_manager(self):
+        return self._shortcuts_manager
+
+    @shortcuts_manager.setter
+    def shortcuts_manager(self, value):
+        self._shortcuts_manager = value
+
+    @GObject.Property(type=Config)
+    def config(self):
+        return self._config
+
+    @config.setter
+    def config(self, value):
+        self._config = value
+
+    def do_activate(self):
+        win = self.props.active_window
+        if not win:
+            win = MainWindow(application=self)
+            self.bind_property("style-manager", win.watch_view.widget_timeline, "style-manager", flags=GObject.BindingFlags.SYNC_CREATE)
+            self.bind_property("shortcuts-manager", win.watch_view, "shortcuts-manager", flags=GObject.BindingFlags.SYNC_CREATE)
+            self.bind_property("config", win.watch_view, "config", flags=GObject.BindingFlags.SYNC_CREATE)
+            self.bind_property("config", win.export_view, "config", flags=GObject.BindingFlags.SYNC_CREATE)
+            self.bind_property("config", win, "config", flags=GObject.BindingFlags.SYNC_CREATE)
+            self.bind_property("shortcuts-manager", win.watch_view, "shortcuts-manager", flags=GObject.BindingFlags.SYNC_CREATE)
+            self.bind_property("shortcuts-manager", win, "shortcuts-manager", flags=GObject.BindingFlags.SYNC_CREATE)
+            self.window = win
+
+            self._shortcuts_manager.init(win.shortcut_controller)
+        win.present()
+
+    def on_shutdown(self, *_args) -> None:
+        for win in self.get_windows():
+            if isinstance(win, Gtk.Window):
+                win.close()
+
+    def on_about_action(self, *args):
+        about = Adw.AboutDialog(application_name='Lada',
+                                application_icon='io.github.ladaapp.lada',
+                                license_type=Gtk.License.AGPL_3_0,
+                                website='https://codeberg.org/ladaapp/lada',
+                                issue_url='https://codeberg.org/ladaapp/lada/issues',
+                                version=VERSION)
+        about.present(self.props.active_window)
+
+    def on_shortcut_action(self, *args):
+        shortcuts_window = ShortcutsWindow(self._shortcuts_manager)
+        shortcuts_window.show()
+
+    def create_action(self, name, callback, shortcuts=None):
+        action = Gio.SimpleAction.new(name, None)
+        action.connect("activate", callback)
+        self.add_action(action)
+        if shortcuts:
+            self.set_accels_for_action(f"app.{name}", shortcuts)

lada/gui/main.py
@@ -5,111 +5,29 @@ import logging
 import pathlib
 import sys
 
-import gi
+from lada import IS_FLATPAK, LOG_LEVEL
 
-from lada import VERSION, LOG_LEVEL
-
-gi.require_version('Gtk', '4.0')
-gi.require_version('Adw', '1')
-gi.require_version('Gst', '1.0')
-gi.require_version('GstApp', '1.0')
-
-from gi.repository import Gtk, Gio, Adw, Gdk, Gst, GObject
-
-Gst.init(None)
+load_fallback = False
+try:
+    import torch
+except ModuleNotFoundError:
+    if IS_FLATPAK:
+        load_fallback = True
+    else:
+        raise
 
 here = pathlib.Path(__file__).parent.resolve()
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=LOG_LEVEL)
 
-from lada.gui.window import MainWindow
-from lada.gui.shortcuts import ShortcutsWindow, ShortcutsManager
-from lada.gui.config.config import Config
-
-class LadaApplication(Adw.Application):
-
-    def __init__(self):
-        super().__init__(application_id='io.github.ladaapp.lada',
-                         flags=Gio.ApplicationFlags.DEFAULT_FLAGS)
-        self.create_action("quit", lambda *_: self.quit(), ("<primary>q",))
-        self.create_action('about', self.on_about_action)
-        self.create_action('shortcuts', self.on_shortcut_action)
-
-        css_provider = Gtk.CssProvider()
-        css_provider.load_from_path(str(here.joinpath('style.css')))
-        Gtk.StyleContext.add_provider_for_display(Gdk.Display.get_default(), css_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
-
-        resource = Gio.resource_load(str(here.joinpath('resources.gresource')))
-        Gio.resources_register(resource)
-
-        self._shortcuts_manager: ShortcutsManager = ShortcutsManager()
-        self._config: Config = Config(self.get_style_manager())
-        self._config.load_config()
-        self.window: MainWindow | None = None
-
-        self.connect("shutdown", self.on_shutdown)
-
-    @GObject.Property(type=ShortcutsManager)
-    def shortcuts_manager(self):
-        return self._shortcuts_manager
-
-    @shortcuts_manager.setter
-    def shortcuts_manager(self, value):
-        self._shortcuts_manager = value
-
-    @GObject.Property(type=Config)
-    def config(self):
-        return self._config
-
-    @config.setter
-    def config(self, value):
-        self._config = value
-
-    def do_activate(self):
-        win = self.props.active_window
-        if not win:
-            win = MainWindow(application=self)
-            self.bind_property("style-manager", win.watch_view.widget_timeline, "style-manager", flags=GObject.BindingFlags.SYNC_CREATE)
-            self.bind_property("shortcuts-manager", win.watch_view, "shortcuts-manager", flags=GObject.BindingFlags.SYNC_CREATE)
-            self.bind_property("config", win.watch_view, "config", flags=GObject.BindingFlags.SYNC_CREATE)
-            self.bind_property("config", win.export_view, "config", flags=GObject.BindingFlags.SYNC_CREATE)
-            self.bind_property("config", win, "config", flags=GObject.BindingFlags.SYNC_CREATE)
-            self.bind_property("shortcuts-manager", win.watch_view, "shortcuts-manager", flags=GObject.BindingFlags.SYNC_CREATE)
-            self.bind_property("shortcuts-manager", win, "shortcuts-manager", flags=GObject.BindingFlags.SYNC_CREATE)
-            self.window = win
-
-            self._shortcuts_manager.init(win.shortcut_controller)
-        win.present()
-
-    def on_shutdown(self, *_args) -> None:
-        for win in self.get_windows():
-            if isinstance(win, Gtk.Window):
-                win.close()
-
-    def on_about_action(self, *args):
-        about = Adw.AboutDialog(application_name='Lada',
-                                application_icon='io.github.ladaapp.lada',
-                                license_type=Gtk.License.AGPL_3_0,
-                                website='https://codeberg.org/ladaapp/lada',
-                                issue_url='https://codeberg.org/ladaapp/lada/issues',
-                                version=VERSION)
-        about.present(self.props.active_window)
-
-    def on_shortcut_action(self, *args):
-        shortcuts_window = ShortcutsWindow(self._shortcuts_manager)
-        shortcuts_window.show()
-
-    def create_action(self, name, callback, shortcuts=None):
-        action = Gio.SimpleAction.new(name, None)
-        action.connect("activate", callback)
-        self.add_action(action)
-        if shortcuts:
-            self.set_accels_for_action(f"app.{name}", shortcuts)
-
-
 def main():
-    app = LadaApplication()
+    if load_fallback:
+        from lada.gui.missing_flatpak_extension_application import MissingFlatpakExtensionApplication
+        app = MissingFlatpakExtensionApplication()
+    else:
+        from lada.gui.application import LadaApplication
+        app = LadaApplication()
     try:
         return app.run(sys.argv)
     except KeyboardInterrupt:

lada/gui/missing_flatpak_extension_application.py
@@ -1,0 +1,82 @@
+# SPDX-FileCopyrightText: Lada Authors
+# SPDX-License-Identifier: AGPL-3.0
+
+import logging
+import pathlib
+
+import gi
+
+gi.require_version('Gtk', '4.0')
+gi.require_version('Adw', '1')
+
+from gi.repository import Gtk, Gio, Adw, Gdk
+
+here = pathlib.Path(__file__).parent.resolve()
+
+from lada import VERSION, LOG_LEVEL
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=LOG_LEVEL)
+
+class MissingFlatpakExtensionApplication(Adw.Application):
+
+    def __init__(self):
+        super().__init__(application_id='io.github.ladaapp.lada',
+                         flags=Gio.ApplicationFlags.DEFAULT_FLAGS)
+        self.create_action("quit", lambda *_: self.quit(), ("<primary>q",))
+        self.create_action('about', self.on_about_action)
+
+        css_provider = Gtk.CssProvider()
+        css_provider.load_from_path(str(here.joinpath('style.css')))
+        Gtk.StyleContext.add_provider_for_display(Gdk.Display.get_default(), css_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+
+        resource = Gio.resource_load(str(here.joinpath('resources.gresource')))
+        Gio.resources_register(resource)
+
+    def on_about_action(self, *args):
+        about = Adw.AboutDialog(application_name='Lada',
+                                application_icon='io.github.ladaapp.lada',
+                                license_type=Gtk.License.AGPL_3_0,
+                                website='https://codeberg.org/ladaapp/lada',
+                                issue_url='https://codeberg.org/ladaapp/lada/issues',
+                                version=VERSION)
+        about.present(self.props.active_window)
+
+    def create_action(self, name, callback, shortcuts=None):
+        action = Gio.SimpleAction.new(name, None)
+        action.connect("activate", callback)
+        self.add_action(action)
+        if shortcuts:
+            self.set_accels_for_action(f"app.{name}", shortcuts)
+
+    def do_activate(self):
+        win = self.props.active_window
+        if not win:
+            win = Adw.ApplicationWindow(application=self, title="Hello World")
+            win.set_title("Lada")
+            win.set_default_size(900, 550)
+
+            status_page = Adw.StatusPage()
+            status_page.props.title = _("No GPU Add-On installed")
+            status_page.props.description = _("In order to use the application you need to install one of Lada's Flatpak Add-Ons that is compatible with your hardware")
+            status_page.set_icon_name("exclamation-mark-symbolic")
+
+            header_bar = Adw.HeaderBar()
+            menu_button = Gtk.MenuButton()
+            menu_button.set_icon_name("open-menu-symbolic")
+            menu = Gio.Menu()
+            menu.append(_("About Lada"), "app.about")
+            menu_button.set_menu_model(menu)
+            header_bar.pack_end(menu_button)
+
+            toolbar_view = Adw.ToolbarView()
+            toolbar_view.add_top_bar(header_bar)
+            toolbar_view.set_content(status_page)
+
+            win.set_content(toolbar_view)
+        win.present()
+
+    def on_shutdown(self, *_args) -> None:
+        for win in self.get_windows():
+            if isinstance(win, Gtk.Window):
+                win.close()

packaging/flatpak/convert-pylock-to-flatpak.py
@@ -63,7 +63,7 @@ def write_flatpak_module_yaml(dependencies: list[tuple[str, str, str]], path: st
 def build_pip_install_commands(dependencies: list[tuple[str, str, str]]) -> list[str]:
     packages = [name for name, _, _ in dependencies]
     return [
-        "pip3 install --verbose --exists-action=i --no-index "
+        "pip3 install --verbose --exists-action=i --no-index --no-deps "
         "--find-links=\"file://${PWD}\" --prefix=${FLATPAK_DEST} "
         f"--no-build-isolation {' '.join(packages)}"
     ]
@@ -136,7 +136,7 @@ def is_compatible_wheel(parsed_wheel, glib_major: int, glib_minor: int, python_m
     for tag in parsed_wheel.platform_tags:
         if tag == "any":
             incompatible = False
-        elif tag.startswith("manylinux") and tag.endswith("x86_64"):
+        elif (tag.startswith("manylinux") or tag.startswith("linux")) and tag.endswith("x86_64"):
             incompatible = False
             result = re.search(r"manylinux_(\d+)_(\d+)_.*", tag)
             if result and len(result.groups()) == 2:
@@ -193,7 +193,7 @@ def get_wheel_or_sdist(compatible_wheels: list, package: dict) -> tuple[str, str
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', type=str, default='-', help="path to pylock.toml file or '-' to read from stdin")
-    parser.add_argument('--output', type=str, default='packaging/flatpak/lada-python-dependencies.yaml')
+    parser.add_argument('--output', type=str)
     parser.add_argument('--gnome-runtime-version', type=int, default=49)
     return parser.parse_args()
 

commit 539f72f187c6e9403a44504db7f2eb9964c56049

packaging: Fix file not found error when using Windows executable
That was a regression introduced by #303.
Previously we loaded style.css and resuorces.gresource in main.py which is also entry point for PyInstaller so its __file__ (here variable) pointed to root.
With that change we moved this into a separte file application.py so __file__ now will be lada/application so we need to adjust target for these files in .spec file accordingly.

packaging/windows/lada.spec
@@ -45,8 +45,8 @@ def get_gui_components(project_root_dir: str, common_datas: list, common_binarie
     gui_datas = common_datas + [
         (str(p), str(p.relative_to(project_root_dir).parent)) for p in (pathlib.Path(project_root_dir) / "lada" / "gui").rglob("*.ui")
     ] + [
-        (ospj(project_root_dir, 'lada/gui/style.css'), '.'),
-        (ospj(project_root_dir, 'lada/gui/resources.gresource'), '.'),
+        (ospj(project_root_dir, 'lada/gui/style.css'), 'lada/gui'),
+        (ospj(project_root_dir, 'lada/gui/resources.gresource'), 'lada/gui'),
         (ospj(project_root_dir, 'assets/io.github.ladaapp.lada.png'), 'share/icons/hicolor/128x128/apps'),
     ]
 